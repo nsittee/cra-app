37f55050dc1ca88ed8256d343764abfd757034c3
nutthawut <nutthawut@email>
2021-06-13 23:25:13 +0700

    Fix script 1125

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,9 +2,4 @@ echo "start server"
 
 whoami
 export PORT=8080
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
-
 npm start
