08462d2ef4c256d07f18eb6a5c9cd5f7a249ca72
nutthawut <nutthawut@email>
2021-06-13 23:17:02 +0700

    Fix script XD

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,8 +1,10 @@
 echo "start server"
+
+whoami
+export PORT=8080
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
-whoami
-export PORT=8080
 npm start
