72402e989fa53680125c646094c25b18d0fe6761
nutthawut <nutthawut@email>
2021-06-13 23:11:43 +0700

    Update script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,8 @@
 echo "start server"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+
 whoami
 export PORT=8080
 npm start
