3e9ae269ff4ab8ff8c48592eba5ac963bc2095a9
nutthawut <nutthawut@email>
2021-07-06 00:45:17 +0700

    202107061245

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -6,6 +6,10 @@ ls
 cd /application
 ls
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+
 export PORT=8080
 npm install
 npm install pm2 -g
