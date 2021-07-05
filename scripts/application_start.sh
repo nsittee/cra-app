90d9aa333aa56c7a95dc113a982dce6a3b3d2faa
nutthawut <nutthawut@email>
2021-07-06 00:33:05 +0700

    Revert "Fix script 0110"
    This reverts commit 2d0d62492c78f3563b456acece1ba9c81d42216b.

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -10,7 +10,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
-export PORT=80
+export PORT=8080
 npm install
 npm install pm2 -g
 npm start
---
+++ scripts/create_test_db.sh
@@ -0,0 +1,1 @@
+echo "create test db"
