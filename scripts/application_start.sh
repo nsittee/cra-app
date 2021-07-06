d63b0a901c0df84369b88daf5582cad3a2ef3452
nutthawut <nutthawut@email>
2021-07-06 19:34:10 +0700

    202107061934

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -9,9 +9,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
 export PORT=8080
-npm install
-npm install pm2 -g
-npm start &
 
 pm2 start npm --name "cra-app" -- start
 pm2 startup
