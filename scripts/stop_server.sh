d2ef1ed4e356bd15c443e5c6fa5e25b420c2e53e
nutthawut <nutthawut@email>
2021-07-06 20:16:44 +0700

    202107062007

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,5 +1,7 @@
 echo "stop server"
 
+echo "check pm2 command"
 ls /home/ec2-user/.nvm/versions/node/v12.19.0/bin/
 
+echo "pm2 stop cra-app"
 /home/ec2-user/.nvm/versions/node/v12.19.0/bin/pm2 stop cra-app
