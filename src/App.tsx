7a80409d910ee2766a60a21f88afea74f3532df6
nutthawut <nutthawut@email>
2021-07-08 00:57:01 +0700

    Test Commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload. PEPESAD PEPELAUGH
+          Edit <code>src/App.tsx</code> and save to reload. PEPESAD PEPELAUGH OMEGALUL
         </p>
         <a
           className="App-link"
