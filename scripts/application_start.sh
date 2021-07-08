490ede4ac700316b13e8803d751a195245907838
nutthawut <nutthawut@email>
2021-07-08 20:42:19 +0700

    Remove and fix some script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 source /home/ec2-user/.bash_profile
 echo "start server"
+cd /application
 
 export PROTOCOL=http
 export PORT=8080
 export EC2_CONTEXT=$PROTOCOL://ec2-13-213-47-102.ap-southeast-1.compute.amazonaws.com:$PORT
 
-npm install
 pm2 start npm --name "cra-app" -- start
 pm2 save
---
+++ scripts/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source /home/ec2-user/.bash_profile
+echo "install"
+cd /application
+
+npm install
