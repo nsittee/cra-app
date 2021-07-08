da923ea502ccc74b25d56b2ceee36344f32438d9
nutthawut <nutthawut@email>
2021-07-08 21:00:34 +0700

    Fix stop script #2

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 source /home/ec2-user/.bash_profile
-echo "stop server"
+echo "stop script"
 
-pm2 stop cra-app
-pm2 delete cra-app
-
-echo "stop"
+pm2 stop cra-app || : echo "pm2 stop"
+pm2 delete cra-app || : echo "pm2 delete"
