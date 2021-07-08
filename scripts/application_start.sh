d5f137c21162c640d0ee1efade76e03fb536bb71
nutthawut <nutthawut@email>
2021-07-08 21:38:43 +0700

    Test env #4

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,7 +4,7 @@ echo "start server"
 cd /application
 
 export PORT=8080
-export PUBLIC_URL='http://ec2-13-213-47-102.ap-southeast-1.compute.amazonaws.com:$PORT'
+export PUBLIC_URL="http://ec2-13-213-47-102.ap-southeast-1.compute.amazonaws.com:$PORT"
 export NODE_ENV=dev
 
 export REACT_APP_NUMBER=1321347
