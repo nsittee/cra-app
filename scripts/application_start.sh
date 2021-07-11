337404502f269d5b3b34602261790cbc7838b05a
nutthawut <nutthawut@email>
2021-07-11 23:58:43 +0700

    Update ignore list and use env file

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,12 +3,5 @@ source /home/ec2-user/.bash_profile
 echo "start server"
 cd /application
 
-export PORT=8080
-export NODE_ENV=dev
-
-export REACT_APP_PUBLIC_URL="http://ec2-13-213-47-102.ap-southeast-1.compute.amazonaws.com:$PORT"
-export REACT_APP_NUMBER=1321347
-export REACT_APP_NAME='Nutthawut'
-
 pm2 start npm --name "cra-app" -- start
 pm2 save
